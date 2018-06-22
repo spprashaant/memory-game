9c9fd1e8f80e61b4a2b51ecfd57b4bda94e14ab3
Prashanth <spprashaant@gmail.com>
2018-06-22 11:56:42 +0530

    Display card symbol on click

--- js/app.js
+++ js/app.js
@@ -39,9 +39,12 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+function displayCardSymbol(card){
+    card.classList.add('match');
+}
 
 function cardClick(){
-    alert('card clicked');
+    displayCardSymbol(this);
 }
 for(const card of cards){
     card.addEventListener('click', cardClick);
